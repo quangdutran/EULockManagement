6fe3ad29d665d8b86ccdd5801b87852b2c1605c1
Du Tran <quangdutran809@gmail.com>
2024-09-14 16:51:27 +0700

    Add profile test using H2 DB

--- src/test/kotlin/com/dutq/lock/management/lock_management/LockManagementApplicationTests.kt
+++ src/test/kotlin/com/dutq/lock/management/lock_management/LockManagementApplicationTests.kt
@@ -2,8 +2,10 @@ package com.dutq.lock.management.lock_management
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LockManagementApplicationTests {
 
 	@Test
