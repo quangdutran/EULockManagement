4d6e03668646ea7a82308d332c5f2ae768cef702
Du Tran <quangdutran809@gmail.com>
2024-09-22 15:14:12 +0700

    Secure password in env file

--- src/main/kotlin/com/dutq/lock/management/lock_management/config/SecurityConfig.java
+++ src/main/kotlin/com/dutq/lock/management/lock_management/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @RequiredArgsConstructor
 @Configuration
